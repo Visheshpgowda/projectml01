e3263f03ef06690a3d56efa8362ec9e28ce0c241
Vishesh_techie <visheshhirisave@gmail.com>
2024-06-17 22:09:28 +0530

    Chore:Added project details

---
+++ main.py
@@ -0,0 +1,514 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import numpy as np # type: ignore
+import matplotlib.pyplot as plt # type: ignore
+from utils import *
+import copy
+import math
+
+
+
+
+
+X_train, y_train = load_data("data/ex2data1.txt")
+
+
+
+
+
+print("First five elements in X_train are:\n", X_train[:5])
+print("Type of X_train:",type(X_train))
+
+
+
+
+
+
+
+print("First five elements in y_train are:\n", y_train[:5])
+print("Type of y_train:",type(y_train))
+
+
+
+
+print ('The shape of X_train is: ' + str(X_train.shape))
+print ('The shape of y_train is: ' + str(y_train.shape))
+print ('We have m = %d training examples' % (len(y_train)))
+
+
+
+
+plot_data(X_train, y_train[:], pos_label="Admitted", neg_label="Not admitted")
+
+# Set the y-axis label
+plt.ylabel('Exam 2 score') 
+# Set the x-axis label
+plt.xlabel('Exam 1 score') 
+plt.legend(loc="upper right")
+plt.show()
+
+
+
+def sigmoid(z):
+    """
+    Compute the sigmoid of z
+
+    Args:
+        z (ndarray): A scalar, numpy array of any size.
+
+    Returns:
+        g (ndarray): sigmoid(z), with the same shape as z
+         
+    """
+          
+    
+    g=1/(1+np.exp(-z))
+     
+    
+    return g
+
+
+# Note: You can edit this value
+value = 0
+
+print (f"sigmoid({value}) = {sigmoid(value)}")
+
+
+
+print ("sigmoid([ -1, 0, 1, 2]) = " + str(sigmoid(np.array([-1, 0, 1, 2]))))
+
+
+def compute_cost(x, y, w, b, *argv):
+    """
+    Computes the cost over all examples
+    Args:
+      X : (ndarray Shape (m,n)) data, m examples by n features
+      y : (ndarray Shape (m,))  target value 
+      w : (ndarray Shape (n,))  values of parameters of the model      
+      b : (scalar)              value of bias parameter of the model
+      *argv : unused, for compatibility with regularized version below
+    Returns:
+      total_cost : (scalar) cost 
+    """
+
+    m, n = x.shape
+    
+    ### START CODE HERE ###
+    total_cost=0
+    for i in range(m):
+        z = np.dot(w, x[i]) + b
+        prediction = sigmoid(z)
+        loss = -y[i] * np.log(prediction) - (1 - y[i]) * np.log(1 - prediction)
+        total_cost+=loss
+    total_cost=total_cost/m
+    
+        
+        
+            
+        
+        
+        
+        
+    
+
+
+    return total_cost
+
+
+
+m, n = X_train.shape
+
+# Compute and display cost with w and b initialized to zeros
+initial_w = np.zeros(n)
+initial_b = 0.
+cost = compute_cost(X_train, y_train, initial_w, initial_b)
+print('Cost at initial w and b (zeros): {:.3f}'.format(cost))
+
+
+
+# In[14]:
+
+
+# Compute and display cost with non-zero w and b
+test_w = np.array([0.2, 0.2])
+test_b = -24.
+cost = compute_cost(X_train, y_train, test_w, test_b)
+
+print('Cost at test w and b (non-zeros): {:.3f}'.format(cost))
+
+
+# UNIT TESTS
+
+
+
+def compute_gradient(x, y, w, b, *argv): 
+    """
+    Computes the gradient for logistic regression 
+ 
+    Args:
+      X : (ndarray Shape (m,n)) data, m examples by n features
+      y : (ndarray Shape (m,))  target value 
+      w : (ndarray Shape (n,))  values of parameters of the model      
+      b : (scalar)              value of bias parameter of the model
+      *argv : unused, for compatibility with regularized version below
+    Returns
+      dj_dw : (ndarray Shape (n,)) The gradient of the cost w.r.t. the parameters w. 
+      dj_db : (scalar)             The gradient of the cost w.r.t. the parameter b. 
+    """
+    m, n = x.shape
+    dj_dw = np.zeros(w.shape)
+    dj_db = 0.
+
+    ### START CODE HERE ### 
+    for i in range(m):
+        z = np.dot(w, x[i]) + b
+        prediction = sigmoid(z)
+        err = prediction-y[i];
+        for j in range(n): 
+            dj_dw[j]=dj_dw[j]+err*x[i,j]
+        dj_db = dj_db + err
+    dj_dw = dj_dw/m                                   #(n,)
+    dj_db = dj_db/m  
+        
+
+    return dj_db, dj_dw
+
+
+
+
+
+
+
+
+test_w = np.array([ 0.2, -0.5])
+test_b = -24
+dj_db, dj_dw  = compute_gradient(X_train, y_train, test_w, test_b)
+
+print('dj_db at test w and b:', dj_db)
+print('dj_dw at test w and b:', dj_dw.tolist())
+
+
+
+
+
+def gradient_descent(X, y, w_in, b_in, cost_function, gradient_function, alpha, num_iters, lambda_): 
+    """
+    Performs batch gradient descent to learn theta. Updates theta by taking 
+    num_iters gradient steps with learning rate alpha
+    
+    Args:
+      X :    (ndarray Shape (m, n) data, m examples by n features
+      y :    (ndarray Shape (m,))  target value 
+      w_in : (ndarray Shape (n,))  Initial values of parameters of the model
+      b_in : (scalar)              Initial value of parameter of the model
+      cost_function :              function to compute cost
+      gradient_function :          function to compute gradient
+      alpha : (float)              Learning rate
+      num_iters : (int)            number of iterations to run gradient descent
+      lambda_ : (scalar, float)    regularization constant
+      
+    Returns:
+      w : (ndarray Shape (n,)) Updated values of parameters of the model after
+          running gradient descent
+      b : (scalar)                Updated value of parameter of the model after
+          running gradient descent
+    """
+    
+    # number of training examples
+    m = len(X)
+    
+    # An array to store cost J and w's at each iteration primarily for graphing later
+    J_history = []
+    w_history = []
+    
+    for i in range(num_iters):
+
+        # Calculate the gradient and update the parameters
+        dj_db, dj_dw = gradient_function(X, y, w_in, b_in, lambda_)   
+
+        # Update Parameters using w, b, alpha and gradient
+        w_in = w_in - alpha * dj_dw               
+        b_in = b_in - alpha * dj_db              
+       
+        # Save cost J at each iteration
+        if i<100000:      # prevent resource exhaustion 
+            cost =  cost_function(X, y, w_in, b_in, lambda_)
+            J_history.append(cost)
+
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i% math.ceil(num_iters/10) == 0 or i == (num_iters-1):
+            w_history.append(w_in)
+            print(f"Iteration {i:4}: Cost {float(J_history[-1]):8.2f}   ")
+        
+    return w_in, b_in, J_history, w_history #return w and J,w history for graphing
+
+
+
+
+np.random.seed(1)
+initial_w = 0.01 * (np.random.rand(2) - 0.5)
+initial_b = -8
+
+# Some gradient descent settings
+iterations = 10000
+alpha = 0.001
+
+w,b, J_history,_ = gradient_descent(X_train ,y_train, initial_w, initial_b, 
+                                   compute_cost, compute_gradient, alpha, iterations, 0)
+
+
+
+plot_decision_boundary(w, b, X_train, y_train)
+# Set the y-axis label
+plt.ylabel('Exam 2 score') 
+# Set the x-axis label
+plt.xlabel('Exam 1 score') 
+plt.legend(loc="upper right")
+plt.show()
+
+
+def predict(x, w, b): 
+    """
+    Predict whether the label is 0 or 1 using learned logistic
+    regression parameters w
+    
+    Args:
+      X : (ndarray Shape (m,n)) data, m examples by n features
+      w : (ndarray Shape (n,))  values of parameters of the model      
+      b : (scalar)              value of bias parameter of the model
+
+    Returns:
+      p : (ndarray (m,)) The predictions for X using a threshold at 0.5
+    """
+    # number of training examples
+    m, n = x.shape   
+    p = np.zeros(m)
+   
+    ### START CODE HERE ### 
+    # Loop over each example
+    for i in range(m):   
+        z = np.dot(w, x[i]) + b
+        prediction = sigmoid(z)
+#         for j in range(n): 
+#             # Add the corresponding term to z_wb
+#             z_wb += None
+        
+        # Add bias term 
+        if(prediction>=0.5):
+            p[i]=1
+        else:
+            p[i]=0
+        
+#         # Calculate the prediction for this example
+#         f_wb = None
+
+#         # Apply the threshold
+#         p[i] = None
+        
+    ### END CODE HERE ### 
+    return p
+
+
+np.random.seed(1)
+tmp_w = np.random.randn(2)
+tmp_b = 0.3    
+tmp_X = np.random.randn(4, 2) - 0.5
+
+tmp_p = predict(tmp_X, tmp_w, tmp_b)
+print(f'Output of predict: shape {tmp_p.shape}, value {tmp_p}')
+
+
+
+
+
+# In[29]:
+
+
+#Compute accuracy on our training set
+p = predict(X_train, w,b)
+print('Train Accuracy: %f'%(np.mean(p == y_train) * 100))
+
+
+X_train, y_train = load_data("data/ex2data2.txt")
+
+
+
+# In[31]:
+
+
+# print X_train
+print("X_train:", X_train[:5])
+print("Type of X_train:",type(X_train))
+
+# print y_train
+print("y_train:", y_train[:5])
+print("Type of y_train:",type(y_train))
+
+
+
+
+print ('The shape of X_train is: ' + str(X_train.shape))
+print ('The shape of y_train is: ' + str(y_train.shape))
+print ('We have m = %d training examples' % (len(y_train)))
+
+
+
+
+# Plot examples
+plot_data(X_train, y_train[:], pos_label="Accepted", neg_label="Rejected")
+
+# Set the y-axis label
+plt.ylabel('Microchip Test 2') 
+# Set the x-axis label
+plt.xlabel('Microchip Test 1') 
+plt.legend(loc="upper right")
+plt.show()
+
+
+
+print("Original shape of data:", X_train.shape)
+
+mapped_X =  map_feature(X_train[:, 0], X_train[:, 1])
+print("Shape after feature mapping:", mapped_X.shape)
+
+
+# Let's also print the first elements of `X_train` and `mapped_X` to see the tranformation.
+
+
+
+
+print("X_train[0]:", X_train[0])
+print("mapped X_train[0]:", mapped_X[0])
+
+
+def compute_cost_reg(x, y, w, b, lambda_ = 1):
+    """
+    Computes the cost over all examples
+    Args:
+      X : (ndarray Shape (m,n)) data, m examples by n features
+      y : (ndarray Shape (m,))  target value 
+      w : (ndarray Shape (n,))  values of parameters of the model      
+      b : (scalar)              value of bias parameter of the model
+      lambda_ : (scalar, float) Controls amount of regularization
+    Returns:
+      total_cost : (scalar)     cost 
+    """
+
+    m, n = x.shape
+    
+    # Calls the compute_cost function that you implemented above
+    cost_without_reg = compute_cost(x, y, w, b) 
+    
+    # You need to calculate this value
+    reg_cost = 0.
+    wj=0
+    ### START CODE HERE ###
+    for i in range(n):
+        wj=(w[i]**2)
+        reg_cost+=wj
+    reg_cost=(lambda_ /(2*m))*reg_cost
+        
+        
+    
+    ### END CODE HERE ### 
+    
+    # Add the regularization cost to get the total cost
+    total_cost = cost_without_reg + reg_cost
+
+    return total_cost
+
+
+
+X_mapped = map_feature(X_train[:, 0], X_train[:, 1])
+np.random.seed(1)
+initial_w = np.random.rand(X_mapped.shape[1]) - 0.5
+initial_b = 0.5
+lambda_ = 0.5
+cost = compute_cost_reg(X_mapped, y_train, initial_w, initial_b, lambda_)
+
+print("Regularized cost :", cost)
+
+# UNIT TEST    
+
+
+
+# UNQ_C6
+def compute_gradient_reg(X, y, w, b, lambda_ = 1): 
+    """
+    Computes the gradient for logistic regression with regularization
+ 
+    Args:
+      X : (ndarray Shape (m,n)) data, m examples by n features
+      y : (ndarray Shape (m,))  target value 
+      w : (ndarray Shape (n,))  values of parameters of the model      
+      b : (scalar)              value of bias parameter of the model
+      lambda_ : (scalar,float)  regularization constant
+    Returns
+      dj_db : (scalar)             The gradient of the cost w.r.t. the parameter b. 
+      dj_dw : (ndarray Shape (n,)) The gradient of the cost w.r.t. the parameters w. 
+
+    """
+    m, n = X.shape
+    
+    dj_db, dj_dw = compute_gradient(X, y, w, b)
+    reg_term = (lambda_ / m) * w
+    
+    # Add the regularization term to the gradient of w
+    dj_dw += reg_term
+    
+    ### END CODE HERE ###         
+        
+    return dj_db, dj_dw
+
+
+
+X_mapped = map_feature(X_train[:, 0], X_train[:, 1])
+np.random.seed(1) 
+initial_w  = np.random.rand(X_mapped.shape[1]) - 0.5 
+initial_b = 0.5
+ 
+lambda_ = 0.5
+dj_db, dj_dw = compute_gradient_reg(X_mapped, y_train, initial_w, initial_b, lambda_)
+
+print(f"dj_db: {dj_db}", )
+print(f"First few elements of regularized dj_dw:\n {dj_dw[:4].tolist()}", )
+
+# UNIT TESTS    
+
+
+
+
+# Initialize fitting parameters
+np.random.seed(1)
+initial_w = np.random.rand(X_mapped.shape[1])-0.5
+initial_b = 1.
+
+# Set regularization parameter lambda_ (you can try varying this)
+lambda_ = 0.01    
+
+# Some gradient descent settings
+iterations = 10000
+alpha = 0.01
+
+w,b, J_history,_ = gradient_descent(X_mapped, y_train, initial_w, initial_b, 
+                                    compute_cost_reg, compute_gradient_reg, 
+                                    alpha, iterations, lambda_)
+
+
+
+
+plot_decision_boundary(w, b, X_mapped, y_train)
+# Set the y-axis label
+plt.ylabel('Microchip Test 2') 
+# Set the x-axis label
+plt.xlabel('Microchip Test 1') 
+plt.legend(loc="upper right")
+plt.show()
+
+p = predict(X_mapped, w, b)
+
+print('Train Accuracy: %f'%(np.mean(p == y_train) * 100))
+
+
